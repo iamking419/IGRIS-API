e7257ce4311608b5d66855b6a05ec023e70cf35f
K I N G <michaelkm555@gmail.com>
2026-06-03 23:06:15 +0100

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres.fuaumfrmxbcegzmwbbfz:Kingis419%40gmail.com@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"
+DATABASE_URL = "postgresql+psycopg://postgres.fuaumfrmxbcegzmwbbfz:Kingis419%40gmail.com@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
